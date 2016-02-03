b18c3a19e8c329fc98d52a47448a5b885e44ea96
Vaughn Ramsey <vramsey20@slp-vramsey20.local>
2016-02-03 15:17:03 -0700

    hello

---
+++ main.py
@@ -0,0 +1,12 @@
+import object
+import subject
+import verb
+def main():
+	inputs = []
+	initial = raw_input("Hi I am ACRONYM")
+	while(1==1):
+		subject = subject.getRandomWord()
+		verb = verb.getRandomWord()
+		object = object.getRandomWord
+		x = raw_input(subject, verb, object)
+		inputs.append(x)
