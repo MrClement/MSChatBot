c63127490057b492f77110fb866f5058c8ab188e
Alex Clement <aclement@kentdenver.org>
2016-02-08 15:02:51 -0700

    A few bugfixes

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import subject
 import verb
 def main():
 	inputs = []
-	initial = raw_input("Hi I am ACRONYM")
+	initial = raw_input("Hi I am ACRONYM \n")
 	while(True):
-		subject = subject.getRandomWord()
-		verb = verb.getRandomWord()
-		object = object.getRandomWord
-		x = raw_input(subject, verb, object)
+		s = subject.getRandomWord()
+		v = verb.getRandomWord()
+		o = object.getRandomWord()
+		x = raw_input("ACRONYM: " + s + " " + v + " " +o + "\nResponse> ")
 		inputs.append(x)
 		if(x=="nobody loves you"):
 			return('nobody loves you either')
+
+if __name__ == "__main__":
+	main()
--- verb.py
+++ verb.py
@@ -3,7 +3,7 @@ f = open("verbs.txt")
 #print (f.read())
 verbs = []
 for word in f:
-    objects.append(word.strip('\n'))
+    verbs.append(word.strip('\n'))
 def getRandomWord():
     t = random.randint(0,79)
     x = verbs[t]
