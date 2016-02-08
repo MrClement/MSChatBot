5efee6430a2a39c269b82a0a4a6470c7a8c03dff
Alex Clement <aclement@kentdenver.org>
2016-02-08 15:15:05 -0700

    names

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import subject
 import verb
 def main():
 	inputs = []
-	initial = raw_input("Hi I am ACRONYM \n")
+	print "Hi I am ACRONYM"
+	name = raw_input("ACRONYM: What is your name? \n")
 	while(True):
 		s = subject.getRandomWord()
 		v = verb.getRandomWord()
 		o = object.getRandomWord()
-		x = raw_input("ACRONYM: " + s + " " + v + " " +o + "\nResponse> ")
+		x = raw_input("ACRONYM: Hey, "+ name + ", I heard that " + s + " " + v + " " +o + "\nResponse> ")
 		inputs.append(x)
 		if(x=="nobody loves you"):
 			return('nobody loves you either')
