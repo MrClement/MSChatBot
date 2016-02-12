faf4430251eb783c465e62bcae032077d373fd32
Jack <jgreco22@Jacks-MacBook-Air.local>
2016-02-12 15:25:16 -0700

    new topics

---
+++ Findword.py
@@ -0,0 +1,17 @@
+import random
+x = raw_input("Enter a word: ")
+
+if x.lower().find("computer")>=0: # x.lower().find("code"), x.lower().find("apple"), x.lower().find("dell"), x.lower().find("agar.io") x.lower().find("terminal") x.lower().find("atom") x.lower().find("sublime"), x.lower().find("maleware"), x.lower().find("tojan"), x.lower().find("worm"), x.lower().find("botnet"), x.lower().find("quine"), x.lower().find("byte"), x.lower().find("ram"), x.lower().find("memory"), x.lower().find("screen"), x.lower().find("chip"), x.lower().find("circit"), x.lower().find("electr")>=0 or x.lower().find("mac"):
+    cList = ["Look at 'Metiocre Electronics' for more! ","Malware installation in proggress. ", "Much tech. ", "The horoscopes predict that computers will soon rise to power. ", "Going to download 42.zip. "]
+    y = random.randint(0,5)
+    print cList[y]
+#
+# if x.lower().find == "robot", "3946", "jude", "bearacat", "logic", "holonomic", "drive", "shooter", "nothing but net", "neat", "dank", "bryan", "rich", "shane", "will", "osama", "risk", "vex", "vrc", "faith christian", "cyberbrains", "grant":
+#     rList = ["3946R is the best! ", "The robots will rise again. ", "The bearacat is coming for you. ", "rekt! ", "MLG rekt! ", "Run skills be excellent. "]
+#     y = random.randint(0,6)
+#     print rList[y]
+#
+# if x.lower().find("body") >= 0 or x.lower().find("dead") >=0 or x.lower().find("kill") >=0 or x.lower().find("shredding") >=0 or x.lower().find("murdering") >=0 or x.lower().find("blood") >=0 or x.lower().find("hide"): #or "taxedermy" or "mardering" or "stab" or "maul" or "bury" or "ditch" or "coffin" or "pauper" or "gun" or "knife" or "massacare" or "chainsaw" or "defacated" or "funeral":
+#     bList = ["Hide the body at midnight. ", "There is a dumpster 0.5 miles from you. ", "You can fool the police. No worries. ", "Don't worry, I know a guy. Seriously I do. Trust me. ", "The minimum bribe should be 100,000$. ", "Use a shovel because it is silent. "]
+#     y = random.randint()
+#     printbrList[y]
