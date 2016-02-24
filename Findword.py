3b155c4e3300e53271dd55d686c1d8d152463313
Jack <jgreco22@Jacks-MacBook-Air.local>
2016-02-24 15:04:34 -0700

    it works

--- Findword.py
+++ Findword.py
@@ -1,17 +1,18 @@
 import random
 x = raw_input("Enter a word: ")
+y = x.lower()
 
-if x.lower().find("computer")>=0: # x.lower().find("code"), x.lower().find("apple"), x.lower().find("dell"), x.lower().find("agar.io") x.lower().find("terminal") x.lower().find("atom") x.lower().find("sublime"), x.lower().find("maleware"), x.lower().find("tojan"), x.lower().find("worm"), x.lower().find("botnet"), x.lower().find("quine"), x.lower().find("byte"), x.lower().find("ram"), x.lower().find("memory"), x.lower().find("screen"), x.lower().find("chip"), x.lower().find("circit"), x.lower().find("electr")>=0 or x.lower().find("mac"):
-    cList = ["Look at 'Metiocre Electronics' for more! ","Malware installation in proggress. ", "Much tech. ", "The horoscopes predict that computers will soon rise to power. ", "Going to download 42.zip. "]
-    y = random.randint(0,5)
-    print cList[y]
-#
-# if x.lower().find == "robot", "3946", "jude", "bearacat", "logic", "holonomic", "drive", "shooter", "nothing but net", "neat", "dank", "bryan", "rich", "shane", "will", "osama", "risk", "vex", "vrc", "faith christian", "cyberbrains", "grant":
-#     rList = ["3946R is the best! ", "The robots will rise again. ", "The bearacat is coming for you. ", "rekt! ", "MLG rekt! ", "Run skills be excellent. "]
-#     y = random.randint(0,6)
-#     print rList[y]
-#
-# if x.lower().find("body") >= 0 or x.lower().find("dead") >=0 or x.lower().find("kill") >=0 or x.lower().find("shredding") >=0 or x.lower().find("murdering") >=0 or x.lower().find("blood") >=0 or x.lower().find("hide"): #or "taxedermy" or "mardering" or "stab" or "maul" or "bury" or "ditch" or "coffin" or "pauper" or "gun" or "knife" or "massacare" or "chainsaw" or "defacated" or "funeral":
-#     bList = ["Hide the body at midnight. ", "There is a dumpster 0.5 miles from you. ", "You can fool the police. No worries. ", "Don't worry, I know a guy. Seriously I do. Trust me. ", "The minimum bribe should be 100,000$. ", "Use a shovel because it is silent. "]
-#     y = random.randint()
-#     printbrList[y]
+if y.find("computer")>=0 or y.find("code")>=0 or y.find("apple")>=0 or y.find("dell")>=0 or y.find("agar.io")>=0 or y.find("terminal")>=0 or y.find("atom")>=0 or y.find("sublime")>=0 or y.find("maleware")>=0 or y.find("tojan")>=0 or y.find("worm")>=0 or y.find("botnet")>=0 or y.find("quine")>=0 or y.find("byte")>=0 or y.find("ram")>=0 or y.find("memory")>=0 or y.find("screen")>=0 or y.find("chip")>=0 or y.find("circit")>=0 or y.find("electr")>=0 or y.find("mac"):
+    cList = ["Look at 'Metiocre Electronics' for more! ", "Malware installation in proggress. ", "Much tech. ", "The horoscopes predict that computers will soon rise to power. ", "Going to download 42.zip. "]
+    r = random.randint(0,5)
+    print cList[r]
+
+if y.find("robot")>=0 or y.find("3946")>=0 or y.find("jude")>=0 or y.find("bearacat")>=0 or y.find("logic")>=0 or y.find("holonomic")>=0 or y.find("drive")>=0 or y.find("shooter")>=0 or y.find("nothing but net")>=0 or y.find("neat")>=0 or y.find("dank")>=0 or y.find("bryan")>=0 or y.find("rich")>=0 or y.find("shane")>=0 or y.find("will")>=0 or y.find("osama")>=0 or y.find("risk")>=0 or y.find("vex")>=0 or y.find("vrc")>=0 or y.find("faith christian")>=0 or y.find("cyberbrains")>=0 or y.find("grant")>=0:
+     rList = ["3946R is the best! ", "The robots will rise again. ", "The bearacat is coming for you. ", "rekt! ", "MLG rekt! ", "Run skills be excellent. "]
+     r = random.randint(0,6)
+     print rList[r]
+
+if y.find("body") >= 0 or y.find("dead") >=0 or y.find("kill") >=0 or y.find("shredding") >=0 or y.find("murdering") >=0 or y.find("blood") >=0 or y.find("hide")>=0 or y.find("taxedermy")>=0 or y.find("murdering")>=0 or y.find("stab")>=0 or y.find("maul")>=0 or y.find("bury")>=0 or y.find("ditch")>=0 or y.find("coffin")>=0 or y.find("pauper")>=0 or y.find("gun")>=0 or y.find("knife")>=0 or y.find("massacare")>=0 or y.find("chainsaw")>=0 or y.find("defacated")>=0 or y.find("funeral")>=0:
+     bList = ["Hide the body at midnight. ", "There is a dumpster 0.5 miles from you. ", "You can fool the police. No worries. ", "Don't worry, I know a guy. Seriously I do. Trust me. ", "The minimum bribe should be 100,000$. ", "Use a shovel because it is silent. "]
+     r = random.randint(0,6)
+     print bList[r]
