2b8c591b33003ccad51813a2d67d343133ebfd0b
Jack Parkhouse <jparkhouse20@jparkhouse20-2.local>
2016-02-24 14:52:01 -0700

    fixed reacuring problem

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import subject
 import verb
 def main():
 	inputs = []
+	fixing_redo_first_after_favorite = 0
 	initial = raw_input("Hi I am ACRONYM \n")
 	while(True):
 		s = subject.getRandomWord()
 		v = verb.getRandomWord()
 		o = object.getRandomWord()
-		x = raw_input("ACRONYM: " + s + " " + v + " " +o + "\nResponse> ")
+		if fixing_redo_first_after_favorite == 0:
+			x = raw_input("ACRONYM: " + s + " " + v + " " +o + "\nResponse> ")
+		else:
+			x = raw_input("")
 		favorites = x.lower().find("favorite")
-		if favorite > -1:
+		if favorites > -1:
 			ice_cream = x.lower().find("ice cream")
 			if ice_cream > -1:
 				print ("my favorite ice cream is chocolate")
@@ -24,7 +28,7 @@ def main():
 			if dinosaur > -1:
 				print ("my favorite dinosaur is the T-rex")
 			country = x.lower().find("country")
-			if countr > -1:
+			if country > -1:
 				print ("my favorite country is the USA")
 			footballteam = x.lower().find("football team")
 			if footballteam > -1:
@@ -35,14 +39,14 @@ def main():
 			state = x.lower().find("state")
 			if state > -1:
 				print ("my favorite state is colorado")
-			subject = x.lower().find("subject)
-			if subject > -1:
+			sub = x.lower().find("subject")
+			if sub > -1:
 				print ("my favorite subject is computer programing")
 			actor = x.lower().find("actor")
 			if actor > -1:
 				print ("my favorite actor is Benedict Cumberbatch")
 			movie = x.lower().find("movie")
-			if movir > -1:
+			if movie > -1:
 				print ("my favorite movie is the Avengers")
 			band = x.lower().find("band")
 			if band > -1:
@@ -53,6 +57,9 @@ def main():
 			zoo = x.lower().find("zoo")
 			if zoo > -1:
 				print ("my favorite zoo is the San Deigo Zoo")
+			fixing_redo_first_after_favorite = 1
+		else:
+			fixing_redo_first_after_favorite = 0
 		inputs.append(x)
 		if(x=="nobody loves you"):
 			return('nobody loves you either')
